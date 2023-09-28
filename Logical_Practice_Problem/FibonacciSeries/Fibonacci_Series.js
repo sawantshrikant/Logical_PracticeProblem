216ccf5ffa143dd674a6e149f76e7958e0e169f2
Shrikant <sawantshrikant07@gmail.com>
2023-09-28 21:06:19 +0530

    [Shrikant] Add: Factorial of given number

--- Logical_Practice_Problem/FibonacciSeries/Fibonacci_Series.js
+++ Logical_Practice_Problem/FibonacciSeries/Fibonacci_Series.js
@@ -13,3 +13,26 @@ for (let i = 1; i <= number; i++) {
 
 
 }
+
+// find the factorial Number
+
+const number1 = 5;
+
+if (number1 < 0) {
+    console.log(`Error! Factorial for negative number not possible`)
+
+}
+else if (number1 === 0) {
+    console.log(`The Factorial of ${number1} is  1.`)
+
+}
+
+else {
+    let fact  = 1 ;
+    for (i=1; i <= number1; i++){
+        fact *=  i;
+
+    }
+    console.log(`The factorial of ${number1} is ${fact}.`);
+    
+}
